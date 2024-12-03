62325b11d4086e5d7f0ef9a21543180e0c0464f6
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-12-03 13:32:32 +0100

    feat: disable some TypeScript ESLint rules
    - Disabled the `@typescript-eslint/no-explicit-any` rule
    - Disabled the `@typescript-eslint/no-empty-object-type` rule
    - Disabled the `@typescript-eslint/no-unsafe-function-type` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,11 @@ export default [
   { languageOptions: { globals: { ...globals.browser } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+    },
+  },
 ]
