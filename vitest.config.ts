7a6b75deff2734d2fd23b9f880f882d9004f6ce9
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-12-15 00:41:38 +0100

    chore(test): add v8 coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [
+        'node_modules/',
+        'dist/',
+        'coverage/**',
+        'benchmarks/**',
+        'scripts/**',
+        '*.config.*',
+        'src/types/**',
+        'tests/**',
+      ],
+    },
     typecheck: {
       enabled: true,
       tsconfig: './tsconfig.test.json',
