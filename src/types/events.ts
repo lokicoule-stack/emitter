15cb9fa426a050ce5c3747180028e5592b93387f
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-12-14 18:53:48 +0100

    refactor(types): remove ErrorEvent from core types

--- src/types/events.ts
+++ src/types/events.ts
@@ -4,13 +4,7 @@ export type EventType = string
 
 export type BaseEvent = Record<EventType, unknown>
 
-export type ErrorEvent = BaseEvent & {
-  error: Error
-  source: string
-  context?: unknown
-}
-
-export type EventMap<T extends BaseEvent = BaseEvent> = { error?: ErrorEvent } & T
+export type EventMap<T extends BaseEvent = BaseEvent> = T
 
 export type EventKey<T extends BaseEvent> = keyof T & EventType
 
--- src/types/main.ts
+++ src/types/main.ts
@@ -1,3 +1,2 @@
 export type { EventEmitter, TypedEmitter } from './emitters'
-export type { ErrorEvent, EventMap } from './events'
 export type { EventHandler, RegularHandler, WildcardHandler } from './handlers'
